4daf5871b08f09bea8cf30b16f6a7cf58b93783a
Anthony1x <antonyturtles@gmail.com>
2024-11-02 10:59:52 +0100

    Change control flow a little bit

--- record_audio.sh
+++ record_audio.sh
@@ -17,10 +17,9 @@ if pgrep pw-record; then
     rm $screenshot
     rm $audio
 else
-    dunstify "Recording..." -r 6969
-
     # Take screenshot and size it down while we're at it
-    scrot -a 1280,1440,2560,1440 - | magick - -resize x600 $screenshot
+    scrot -a 1280,1440,2560,1440 - | magick - -resize x600 $screenshot &
 
-    pw-record --target 57 $audio
+    pw-record --target 57 $audio &
+    dunstify "Recording..." -r 6969
 fi
